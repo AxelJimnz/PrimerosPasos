64bf7e41f61dc97fee4a9bd5e43a598e1c1899b8
JP <axel_jimnz5p@outlook.com>
2023-01-25 18:34:25 -0600

    Cree clases para mipular cadenas y modifique ManipulaCadenas.java

--- src/ManipulaCadenas.java
+++ src/ManipulaCadenas.java
@@ -1,12 +1,17 @@
 public class ManipulaCadenas {
     public static void main(String[] args) {
-        String nombre = "ALEXANDRA";
+        String nombre = "AXEL JIMENEZ";
 
         System.out.println("Su nombre es: " +nombre);
 
         System.out.println("Su nombre tiene: " +nombre.length()+ " letras");
 
+        System.out.println("La primera letra de " +nombre+ " es: " +nombre.charAt(0));
         
+        int ultima_letra;
+        ultima_letra = nombre.length();
+
+        System.out.println("La ultima letra de " +nombre+ " es: " +nombre.charAt(ultima_letra-1));
     }
     
 }
